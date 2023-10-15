177b5f2fe5b97749f6a678d39e371b7121657865
nyeinminhtet <nyeinmg904@gmail.com>
2023-10-15 21:33:44 +0630

    chore

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SERVER =
   process.env.NODE_ENV === "production"
-    ? process.env.SERVER
+    ? "https://canvas-drawing.onrender.com"
     : "http://localhost:5000";
 
 export const socket = io(SERVER, { transports: ["websocket"] });
